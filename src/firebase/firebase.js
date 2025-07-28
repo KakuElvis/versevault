2b5dbbe4772b5be630e10fe42fb7c6237511630d
KakuElvis <kakuelvis@gmail.com>
2025-07-28 23:25:01 +0100

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlNF_iJ0er_yt1Uc3cgueppAXULy-Y7Ks",
-  authDomain: "versevault-blurb.firebaseapp.com",
-  projectId: "versevault-blurb",
-  storageBucket: "versevault-blurb.firebasestorage.app",
-  messagingSenderId: "559245778991",
-  appId: "1:559245778991:web:148608eff0e8c6213726da",
-  measurementId: "G-S8JH0NPG2Z"
+  
 };
 
 const app = initializeApp(firebaseConfig);
